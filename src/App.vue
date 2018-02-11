957cbc537ac58038205480deddee265a90ef2813
Carlino <carlinoo@icloud.com>
2018-02-11 02:07:06 +0000

    added login

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
   <v-app>
-    <router-view/>
+    <!-- <v-header></v-header> -->
+    <v-toolbar app color="blue darken-3"
+      dark
+      clipped-left
+      fixed>
+      <v-toolbar-title class="ml-0 pl-3">
+        <span class="hidden-xs-only">WebDep - CoderDojo</span>
+      </v-toolbar-title>
+
+      <div class="d-flex align-center" style="margin-left: auto">
+        <v-btn  href="/" icon v-if="!this.user_signed_in()">
+          <v-icon>edit</v-icon>
+        </v-btn>
+        <v-btn icon v-if="this.user_signed_in()">
+          <v-icon href="/">exit_to_app</v-icon>
+        </v-btn>
+      </div>
+
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+
   </v-app>
 </template>
 
+
 <script>
+
+import Header from './components/Layout/Header';
+
   export default {
     data () {
       return {
 
       }
+    },
+    components: {
+      'v-header': Header
     }
   }
 </script>
--- src/components/User/LogIn.vue
+++ src/components/User/LogIn.vue
@@ -1,16 +1,52 @@
 <template>
-  <h3>Log In</h3>
+  <v-flex xs12 sm6 offset-sm3 back>
+    <br><br>
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Log In</h3>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+      label="Username"
+      v-model="username"
+    ></v-text-field>
+    <v-text-field
+      label="Password"
+      v-model="password"
+    ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn v-on:click="login" flat color="blue darken-3">Log In</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
 </template>
 
 <script>
   export default {
     data () {
       return {
-
+        username: '',
+        password: ''
       }
     },
-    created() {
-      // console.log(this.confirm_user('as', '613666'));
+    methods: {
+      login: function() {
+        this.login_user(this.username, this.password);
+
+        if (this.user_signed_in()) {
+          this.$router.push('/');
+        }
+      }
     }
   }
 </script>
+
+<style media="screen" >
+  .content {
+    background-color: #C9D3D8;
+  }
+
+</style>
