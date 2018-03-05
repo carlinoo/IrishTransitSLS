9b4b9bd71229b540b870f20607637695d569c0a4
Carlino <carlinoo@icloud.com>
2018-03-05 12:26:47 +0000

    added all messages

--- src/components/Messages/AllMessages.vue
+++ src/components/Messages/AllMessages.vue
@@ -2,21 +2,23 @@
   <div>
     <!--  For the list of users -->
     <v-flex xs12 sm6 offset-sm3>
-      <v-list two-line>
-        <template v-for="(item, index) in items.slice(0, 6)">
-          <v-subheader v-if="item.header" :key="item.header">All Messages</v-subheader>
-          <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-          <v-list-tile avatar v-else @click="" :key="item.title">
+      <v-list two-line subheader>
+          <v-subheader inset>All Messages</v-subheader>
+          <v-list-tile avatar v-for="item in items" :key="item.id" @click="go_to_message(item.id)">
             <v-list-tile-avatar>
-              <img :src="item.avatar" >
+              <v-icon class="grey lighten-1 white--text">fingerprint</v-icon>
             </v-list-tile-avatar>
             <v-list-tile-content>
-              <v-list-tile-title v-html="item.title"></v-list-tile-title>
-              <v-list-tile-sub-title><span class='text--primary'>{{ item.from }}</span>: {{ item.subtitle }}</v-list-tile-sub-title>
+              <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ item.last_message }}</v-list-tile-sub-title>
             </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">info</v-icon>
+              </v-btn>
+            </v-list-tile-action>
           </v-list-tile>
-        </template>
-      </v-list>
+        </v-list>
   </v-flex>
   </div>
 </template>
@@ -30,29 +32,27 @@
     data() {
       return {
         items: [
-          { header: 'Today' },
-          { avatar: '/static/doc-images/lists/1.jpg', title: 'Brunch this weekend?', from: "Ali Connors", subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-          { divider: true, inset: true },
-          { avatar: '/static/doc-images/lists/2.jpg', title: 'Summer BBQ', from: "Me", subtitle: "Wish I could come, but I'm out of town this weekend." },
-          { divider: true, inset: true },
-          { avatar: '/static/doc-images/lists/3.jpg', title: 'Oui oui', from: "Sandra Adams", subtitle: "Do you have Paris recommendations? Have you ever been?" },
-          { divider: true, inset: true },
-          { avatar: '/static/doc-images/lists/4.jpg', title: 'Birthday gift', from: "Trevor Hansen", subtitle: "Have any ideas about what we should get Heidi for her birthday?" },
-          { divider: true, inset: true },
-          { avatar: '/static/doc-images/lists/5.jpg', title: 'Recipe to try', from: "Britta Holt", subtitle: "We should eat this: Grate, Squash, Corn, and tomatillo Tacos." }
+
         ]
       }
     },
+
+    methods: {
+      go_to_message(id) {
+        this.$router.push("message/" + id);
+      }
+    },
+
 
     created() {
       var chat = firebase.database().ref('chat');
 
       var rooms = chat.child('room_names');
 
-
       // Create a new ref and log it’s push key
-      rooms.on('value', function (snap) {
-       console.log(snap.val()); // Keep the local user object synced with the Firebase userRef
+      rooms.on('value', (snap) => {
+       this.items = snap.val();
+       console.log(snap.val());
       });
     }
   }
