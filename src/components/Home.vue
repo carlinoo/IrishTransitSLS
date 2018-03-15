a31ef1c6cc8b766e177b689a3473574dbea3c915
Carlino <carlinoo@icloud.com>
2018-03-15 17:21:54 +0000

    added clone database

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,92 @@
 <template>
   <div>
-    <h3>{{ dog }}</h3>
-  </div>
 
+  </div>
 </template>
+
+
 
 <script>
   export default {
     data() {
       return {
-        dog: ''
+        bus_stops: [],
+        bus_routes: []
       }
     },
 
     created() {
-      var vm = this;
 
-      var usersRef = firebase.database().ref('users/');
 
-      var userRef = usersRef.child(firebase.auth().currentUser.uid);
+      var infoRef = firebase.database().ref('information');
 
+      var BusRoutes = infoRef.child('BusRoutes');
 
       // Create a new ref and log it’s push key
-      userRef.on('value', function (snap) {
-       vm.dog = snap.val(); // Keep the local user object synced with the Firebase userRef
+      BusRoutes.on('value', (snap) => {
+       this.bus_routes = snap.val();
       });
 
+      var BusStops = infoRef.child('BusStops');
+
+      // Create a new ref and log it’s push key
+      BusStops.on('value', (snap) => {
+       this.bus_stops = snap.val();
+      });
+
+    },
+
+    methods: {
+
+      setBusRoutes() {
+        var vm = this;
+
+
+        this.$http.get(this.corsAnywhere("https://data.dublinked.ie/cgi-bin/rtpi/routelistinformation")).then(response => {
+
+          response.body.results.forEach(item => {
+
+            this.$http.get(this.corsAnywhere("https://data.dublinked.ie/cgi-bin/rtpi/routeinformation?routeid=" + item.route + "&operator=" + item.operator)).then(data => {
+              console.log(data.body);
+              var infoRef = firebase.database().ref('information');
+
+              var BusRoutes = infoRef.child('BusRoutes');
+
+              if (data.body.results.length != 0) {
+                BusRoutes.child(item.route).set({
+                  operator: item.operator,
+                  info: data.body.results[0].origin + ' / ' + data.body.results[0].destination,
+                  irish_info: data.body.results[0].originlocalized + ' / ' + data.body.results[0].destinationlocalized
+                });
+              }
+            });
+          });
+
+        });
+      },
+
+      setBusStops() {
+        var vm = this;
+        var infoRef = firebase.database().ref('information');
+
+        var BusStops = infoRef.child('BusStops');
+
+        this.$http.get(this.corsAnywhere("https://data.dublinked.ie/cgi-bin/rtpi/busstopinformation")).then(response => {
+
+          response.body.results.forEach(item => {
+
+            BusStops.child(item.stopid).set({
+              name: item.shortname,
+              irish_name: item.shortnamelocalized,
+              longitude: item.longitude,
+              latitude: item.latitude
+            });
+            console.log(item);
+          });
+
+        });
+
+      }
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ Vue.use(Vuetify);
 Vue.use(VueResource);
 Vue.use(AsyncComputed);
 
+
 var sha256 = require('js-sha256').sha256;
 
 
@@ -23,6 +24,9 @@ Vue.mixin({
     getChatroomID: function(user1, user2) {
       var room = (user1 > user2) ? (user2 + '_' + user1) : (user1 + '_' + user2);
       return sha256(room);
+    },
+    corsAnywhere: function(string) {
+      return "https://cors-anywhere.herokuapp.com/" + string
     }
   }
 });
